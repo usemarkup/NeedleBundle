90a6de6f7d38f18d99be7b13cbd3bc1b85cb4199
Douglas Greenshields <dgreenshields@gmail.com>
2014-01-17 00:08:55 +0000

    add null filter provider

---
+++ Tests/Provider/NullFilterProviderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Markup\NeedleBundle\Tests\Provider;
+
+use Markup\NeedleBundle\Provider\NullFilterProvider;
+
+/**
+ * Test for null filter provider.
+ */
+class NullFilterProviderTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->provider = new NullFilterProvider();
+    }
+
+    public function testIsFilterProvider()
+    {
+        $this->assertInstanceOf('Markup\NeedleBundle\Provider\FilterProviderInterface', $this->provider);
+    }
+
+    public function testGetFilterByNameReturnsFalse()
+    {
+        $this->assertFalse($this->provider->getFilterByName('filter'));
+    }
+}
