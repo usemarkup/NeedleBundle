403678bf868e1fb2024c2c3e49fe4f3442173048
Douglas Greenshields <dgreenshields@gmail.com>
2014-02-24 16:59:39 +0000

    add configured interceptor provider/ interceptor configurator

---
+++ Tests/Intercept/InterceptorConfiguratorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Markup\NeedleBundle\Tests\Intercept;
+
+use Markup\NeedleBundle\Intercept\InterceptorConfigurator;
+use Mockery as m;
+
+class InterceptorConfiguratorTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->configurator = new InterceptorConfigurator();
+    }
+
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    public function testIsConfigurator()
+    {
+        $this->assertInstanceOf('Markup\NeedleBundle\Intercept\InterceptorConfiguratorInterface', $this->configurator);
+    }
+
+    public function testInterceptMappersAddedToInterceptor()
+    {
+        $mapper1 = m::mock('Markup\NeedleBundle\Intercept\TypedInterceptMapperInterface')->shouldReceive('getType')->andReturn('type1')->getMock();
+        $mapper2 = m::mock('Markup\NeedleBundle\Intercept\TypedInterceptMapperInterface')->shouldReceive('getType')->andReturn('type2')->getMock();
+        $this->configurator->addInterceptMapper($mapper1);
+        $this->configurator->addInterceptMapper($mapper2);
+        $interceptor = m::mock('Markup\NeedleBundle\Intercept\Interceptor');
+        $interceptor
+            ->shouldReceive('addInterceptMapper')
+            ->twice();
+        $this->configurator->configureInterceptor($interceptor, array());
+    }
+
+    public function testAddDefinitionToInterceptor()
+    {
+        $config = array(
+            'sale' => array(
+                'terms' => array('sale'),
+                'type' => 'route',
+                'route' => 'sale',
+            ),
+        );
+        $interceptor = m::mock('Markup\NeedleBundle\Intercept\Interceptor');
+        $interceptor
+            ->shouldReceive('addDefinition')
+            ->once();
+        $this->configurator->configureInterceptor($interceptor, $config);
+    }
+}
