200a616894dbf4c8eae8a571c2432f89c128331a
Douglas Greenshields <dgreenshields@gmail.com>
2014-02-26 11:55:35 +0000

    add a simple config factory service

---
+++ Config/ContextConfigurationFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Markup\NeedleBundle\Config;
+
+/**
+ * Simple factory class to create a config object from a hash.
+ */
+class ContextConfigurationFactory
+{
+    /**
+     * @param array $configHash
+     * @return ContextConfigurationInterface
+     */
+    public function createConfigFromHash(array $configHash)
+    {
+        return new ContextConfiguration($configHash);
+    }
+} 
