06546766bb7c154f715d5951d5295016049ebb47
Douglas Greenshields <douglas@usemarkup.com>
2015-08-26 13:50:41 +0200

    add callable type hint/ remove explicit check

--- Facet/FacetValue.php
+++ Facet/FacetValue.php
@@ -24,13 +24,10 @@ class FacetValue implements FacetValueInterface
      * @param int    $count
      * @param callable $displayStrategy
      **/
-    public function __construct($value, $count, $displayStrategy = null)
+    public function __construct($value, $count, callable $displayStrategy = null)
     {
         $this->value = $value;
         $this->count = $count;
-        if (null !== $displayStrategy && !is_callable($displayStrategy)) {
-            throw new \InvalidArgumentException('displayStrategy parameter must be callable if set.');
-        }
         $this->displayStrategy = $displayStrategy ?: function ($value) {
             return $value;
         };
