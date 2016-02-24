d9785f8ddec263e3960b4d54662c27e19f2be2c7
Jamie Balish <jamie@usemarkup.com>
2016-02-24 16:31:41 +0000

    fix: typo in class name

--- Tests/Terms/SolrTermsResultTest.php
+++ Tests/Terms/SolrTermsResultTest.php
@@ -6,7 +6,7 @@ use Markup\NeedleBundle\Suggest\SolrSuggestResult;
 use Markup\NeedleBundle\Terms\SolrTermsResult;
 use Mockery as m;
 
-class SolrSuggestResultTest extends \PHPUnit_Framework_TestCase
+class SolrTermsResultTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
