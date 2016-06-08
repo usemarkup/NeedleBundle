3be4e0b03870a627c51e1d2d7b48e2d08289fc0d
Douglas Greenshields <douglas@usemarkup.com>
2016-06-08 13:25:59 +0100

    feat: expose original select query from resolved query

--- Tests/Query/ResolvedSelectQueryInterfaceTest.php
+++ Tests/Query/ResolvedSelectQueryInterfaceTest.php
@@ -32,6 +32,7 @@ class ResolvedSelectQueryInterfaceTest extends \PHPUnit_Framework_TestCase
             'getWhetherFacetIgnoresCurrentFilters',
             'getBoostQueryFields',
             'shouldUseFacetValuesForRecordedQuery',
+            'getOriginalSelectQuery',
             'getRecord',
             'getGroupingField',
             'getGroupingSortCollection'
