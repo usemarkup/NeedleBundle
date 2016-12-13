1cf522d663c5733666e62b2a8072a69e40a7f612
Calum Brodie <calum@usemarkup.com>
2016-12-13 14:56:59 +0000

    feat(attribute): adds specialized attribute decorators

---
+++ Attribute/FloatSpecializedAttributeDecorator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Markup\NeedleBundle\Attribute;
+
+/**
+* A decorator for an attribute that declares a float type (clocking any underlying type).
+*/
+class FloatSpecializedAttributeDecorator extends SpecializedAttributeDecorator implements FloatAttributeInterface
+{}
---
+++ Tests/Attribute/FloatSpecializedAttributeDecoratorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Markup\NeedleBundle\Tests\Filter;
+
+use Markup\NeedleBundle\Attribute\FloatAttributeInterface;
+use Markup\NeedleBundle\Attribute\FloatSpecializedAttributeDecorator;
+use Markup\NeedleBundle\Attribute\SpecializedAttributeInterface;
+use Mockery as m;
+
+/**
+* A test for a decorator for a filter that declares a Boolean type (clocking any underlying type).
+*/
+class FloatSpecializedAttributeDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SpecializedAttributeInterface|m\Mock
+     */
+    private $filter;
+
+    /**
+     * @var FloatSpecializedAttributeDecorator
+     */
+    private $decorator;
+
+
+    public function setUp()
+    {
+        $this->filter = m::mock(SpecializedAttributeInterface::class);
+        $this->decorator = new FloatSpecializedAttributeDecorator($this->filter);
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testIsSpecializedAttribute()
+    {
+        $this->assertInstanceOf(SpecializedAttributeInterface::class, $this->decorator);
+    }
+
+    public function testIsBooleanAttribute()
+    {
+        $this->assertInstanceOf(FloatAttributeInterface::class, $this->decorator);
+    }
+
+    public function testOneToOneDecoration()
+    {
+        $name = 'filter';
+        $displayName = 'Filter';
+        $searchKey = 'fil_ter';
+        $this->filter
+            ->shouldReceive('getName')
+            ->andReturn($name);
+
+        $this->filter
+            ->shouldReceive('getDisplayName')
+            ->andReturn($displayName);
+
+        $this->filter
+            ->shouldReceive('getSearchKey')
+            ->andReturn($searchKey);
+
+        $this->assertEquals($name, $this->decorator->getName());
+        $this->assertEquals($displayName, $this->decorator->getDisplayName());
+        $this->assertEquals($searchKey, $this->decorator->getSearchKey());
+    }
+}
