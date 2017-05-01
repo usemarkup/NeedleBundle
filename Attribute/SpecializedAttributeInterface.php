9f2ed25c590da27e113be387923abd707411a1ef
Calum Brodie <calum@usemarkup.com>
2017-05-01 14:07:05 +0100

    feat: support multiple attribute specializations

--- Attribute/SpecializedAttributeInterface.php
+++ Attribute/SpecializedAttributeInterface.php
@@ -3,23 +3,25 @@
 namespace Markup\NeedleBundle\Attribute;
 
 /**
- * An attribute implementation that has a specialization set on it
+ * An attribute implementation that has one or more specializations set on it
  * allowing the search key to be changed by adding a context
  */
 interface SpecializedAttributeInterface
 {
     /**
-     * @return AttributeSpecialization
+     * @return AttributeSpecialization[]
      */
-    public function getSpecialization();
+    public function getSpecializations();
 
     /**
      * @param AttributeSpecializationContextInterface
+     * @param string
      */
-    public function setContext(AttributeSpecializationContextInterface $context);
+    public function setContext(AttributeSpecializationContextInterface $context, string $specialization);
 
     /**
+     * @param string
      * @return AttributeSpecializationContextInterface
      */
-    public function getContext();
+    public function getContext(string $specialization);
 }
