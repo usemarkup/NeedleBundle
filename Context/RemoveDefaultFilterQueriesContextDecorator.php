185d9f16247d7b60b62cd6ec5693e6d517901c71
Douglas Greenshields <douglas@usemarkup.com>
2018-03-14 12:54:08 +0000

    refactor: add a DecorateContextTrait

--- Context/RemoveDefaultFilterQueriesContextDecorator.php
+++ Context/RemoveDefaultFilterQueriesContextDecorator.php
@@ -10,19 +10,16 @@ use Markup\NeedleBundle\Query\SelectQueryInterface;
  */
 class RemoveDefaultFilterQueriesContextDecorator implements SearchContextInterface
 {
-    const LARGE_NUMBER = 10000;
+    use DecorateContextTrait;
 
-    /**
-     * @var SearchContextInterface
-     **/
-    private $searchContext;
+    const LARGE_NUMBER = 10000;
 
     /**
      * @param SearchContextInterface $searchContext The search context being decorated.
      **/
     public function __construct(SearchContextInterface $searchContext)
     {
-        $this->searchContext = $searchContext;
+        $this->context = $searchContext;
     }
 
     /**
@@ -36,88 +33,8 @@ class RemoveDefaultFilterQueriesContextDecorator implements SearchContextInterfa
     /**
      * {@inheritdoc}
      */
-    public function getDefaultSortCollectionForQuery(SelectQueryInterface $query)
-    {
-        return $this->searchContext->getDefaultSortCollectionForQuery($query);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getItemsPerPage()
     {
         return self::LARGE_NUMBER;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFacets()
-    {
-        return $this->searchContext->getFacets();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getSetDecoratorForFacet(AttributeInterface $facet)
-    {
-        return $this->searchContext->getSetDecoratorForFacet($facet);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getWhetherFacetIgnoresCurrentFilters(AttributeInterface $facet)
-    {
-        return $this->searchContext->getWhetherFacetIgnoresCurrentFilters($facet);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAvailableFilterNames()
-    {
-        return $this->searchContext->getAvailableFilterNames();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBoostQueryFields()
-    {
-        return $this->searchContext->getBoostQueryFields();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFacetCollatorProvider()
-    {
-        return $this->searchContext->getFacetCollatorProvider();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFacetSortOrderProvider()
-    {
-        return $this->searchContext->getFacetSortOrderProvider();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getInterceptor()
-    {
-        return $this->searchContext->getInterceptor();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function shouldRequestFacetValueForMissing()
-    {
-        return $this->searchContext->shouldRequestFacetValueForMissing();
     }
 }
--- Context/UseAvailableFiltersAsFacetsContextDecorator.php
+++ Context/UseAvailableFiltersAsFacetsContextDecorator.php
@@ -11,27 +11,18 @@ use Markup\NeedleBundle\Query\SelectQueryInterface;
  */
 class UseAvailableFiltersAsFacetsContextDecorator implements SearchContextInterface
 {
-    /**
-     * The context being decorated.
-     *
-     * @var SearchContextInterface
-     **/
-    private $searchContext;
+    use DecorateContextTrait;
 
     /**
      * @var FacetProviderInterface
      **/
     private $facetProvider;
 
-    /**
-     * @param SearchContextInterface $searchContext
-     * @param FacetProviderInterface $facetProvider
-     **/
     public function __construct(
         SearchContextInterface $searchContext,
         FacetProviderInterface $facetProvider
     ) {
-        $this->searchContext = $searchContext;
+        $this->context = $searchContext;
         $this->facetProvider = $facetProvider;
     }
 
@@ -46,93 +37,5 @@ class UseAvailableFiltersAsFacetsContextDecorator implements SearchContextInterf
         }
 
         return $facets;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getItemsPerPage()
-    {
-        return $this->searchContext->getItemsPerPage();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getDefaultFilterQueries()
-    {
-        return $this->searchContext->getDefaultFilterQueries();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getDefaultSortCollectionForQuery(SelectQueryInterface $query)
-    {
-        return $this->searchContext->getDefaultSortCollectionForQuery($query);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getSetDecoratorForFacet(AttributeInterface $facet)
-    {
-        return $this->searchContext->getSetDecoratorForFacet($facet);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getWhetherFacetIgnoresCurrentFilters(AttributeInterface $facet)
-    {
-        return $this->searchContext->getWhetherFacetIgnoresCurrentFilters($facet);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAvailableFilterNames()
-    {
-        return $this->searchContext->getAvailableFilterNames();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBoostQueryFields()
-    {
-        return $this->searchContext->getBoostQueryFields();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFacetCollatorProvider()
-    {
-        return $this->searchContext->getFacetCollatorProvider();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFacetSortOrderProvider()
-    {
-        return $this->searchContext->getFacetSortOrderProvider();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getInterceptor()
-    {
-        return $this->searchContext->getInterceptor();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function shouldRequestFacetValueForMissing()
-    {
-        return $this->searchContext->shouldRequestFacetValueForMissing();
     }
 }
