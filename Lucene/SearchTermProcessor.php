174ed731bfea45f02dd7dd91774e9dabd05c2448
Douglas Greenshields <douglas@usemarkup.com>
2018-03-15 17:54:49 +0000

    feat: implement a search term processor for lucene search terms, with normalization and fuzzy matching as options

---
+++ Lucene/SearchTermProcessor.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Markup\NeedleBundle\Lucene;
+
+/**
+ * An object that can normalize/ transform a literal search term into a form to be passed to a backend engine using Lucene syntax.
+ */
+class SearchTermProcessor
+{
+    /**
+     * This filter cannot be combined with others using bitwise logic - that would make no sense anyway.
+     */
+    const FILTER_NONE = 0;
+
+    /**
+     * These filters can be combined to form a bitmask.
+     */
+    const FILTER_NORMALIZE = 1;
+    const FILTER_FUZZY_MATCHING = 2;
+
+    public function process(string $text, int $filterBitMask = self::FILTER_NORMALIZE): string
+    {
+        if ($filterBitMask === self::FILTER_NONE) {
+            return $text;
+        }
+
+        if ($filterBitMask & self::FILTER_NORMALIZE) {
+            $text = trim(preg_replace('/[\:\s~]+/', ' ', $text));
+        }
+
+        if ($filterBitMask & self::FILTER_FUZZY_MATCHING) {
+            $text = preg_replace(
+                [
+                    '/("[\pL\s]+"|\pL+~*)/u',
+                    '/~+/'
+                ],
+                [
+                    '$1~',
+                    '~'
+                ],
+                $text
+            );
+        }
+
+        return $text;
+    }
+}
---
+++ Tests/Lucene/SearchTermProcessorTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Markup\NeedleBundle\Tests\Lucene;
+
+use Markup\NeedleBundle\Lucene\SearchTermProcessor;
+use PHPUnit\Framework\TestCase;
+
+class SearchTermProcessorTest extends TestCase
+{
+    /**
+     * @var SearchTermProcessor
+     */
+    private $processor;
+
+    protected function setUp()
+    {
+        $this->processor = new SearchTermProcessor();
+    }
+
+    public function testProcessWithNoTransformSetPerformsNoTransform()
+    {
+        $text = 'ghost+ "joker~ hundred"';
+        $this->assertEquals($text, $this->processor->process($text, SearchTermProcessor::FILTER_NONE));
+    }
+
+    /**
+     * @dataProvider normalizeCases
+     */
+    public function testProcessWithNormalization(string $input, string $output)
+    {
+        $this->assertEquals(
+            $output,
+            $this->processor->process($input, SearchTermProcessor::FILTER_NORMALIZE)
+        );
+    }
+
+    /**
+     * @dataProvider normalizeCases
+     */
+    public function testProcessesWithNormalizationByDefault(string $input, string $output)
+    {
+        $this->assertEquals(
+            $output,
+            $this->processor->process($input)
+        );
+    }
+
+    public function normalizeCases()
+    {
+        return [
+            ['red:case', 'red case'],
+            ['red~ cane~', 'red cane'],
+            ['"red cane"', '"red cane"'],
+            ['"red cane"~', '"red cane"'],
+            ['red  handle', 'red handle'],
+            ['röda  bilar', 'röda bilar'],
+        ];
+    }
+
+    /**
+     * @dataProvider normalizeAndFuzzyMatchCases
+     */
+    public function testProcessesWithNormalizationAndFuzzyMatching(string $input, string $output)
+    {
+        $this->assertEquals(
+            $output,
+            $this->processor->process(
+                $input,
+                SearchTermProcessor::FILTER_NORMALIZE | SearchTermProcessor::FILTER_FUZZY_MATCHING
+            )
+        );
+    }
+
+    public function normalizeAndFuzzyMatchCases()
+    {
+        return [
+            ['blue shirts', 'blue~ shirts~'],
+            ['"blue shirts"', '"blue shirts"~'],
+            ['blue~ shirts~', 'blue~ shirts~'],
+            ['"blue shirts"~', '"blue shirts"~'],
+            ['"blåa skjortor"~', '"blåa skjortor"~'],
+            ['blåa skjortor', 'blåa~ skjortor~'],
+        ];
+    }
+}
