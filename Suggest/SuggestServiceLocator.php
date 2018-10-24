bd71d3b1502caea1fbe11607a242e8f13cb2dc63
Douglas Greenshields <douglas@usemarkup.com>
2018-10-24 13:31:01 +0100

    feat: make suggest service locator provide on a per corpus basis

--- Suggest/SuggestServiceLocator.php
+++ Suggest/SuggestServiceLocator.php
@@ -4,9 +4,24 @@ declare(strict_types=1);
 
 namespace Markup\NeedleBundle\Suggest;
 
+use Markup\NeedleBundle\Corpus\CorpusBackendProvider;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 
 class SuggestServiceLocator extends ServiceLocator
 {
+    /**
+     * @var CorpusBackendProvider
+     */
+    private $backendProvider;
 
+    public function __construct(array $factories, CorpusBackendProvider $backendProvider)
+    {
+        parent::__construct($factories);
+        $this->backendProvider = $backendProvider;
+    }
+
+    public function getSuggesterForCorpus(string $corpus): SuggestServiceInterface
+    {
+        return $this->get($this->backendProvider->getBackendForCorpus($corpus));
+    }
 }
