88d193a03e0c639758fe36d974866271bf2819d8
Douglas Greenshields <dgreenshields@gmail.com>
2018-11-05 15:35:33 -0500

    refactor: simplify solarium facet set iterator

--- Result/SolariumFacetSetAdaptingIterator.php
+++ Result/SolariumFacetSetAdaptingIterator.php
@@ -32,45 +32,21 @@ class SolariumFacetSetAdaptingIterator implements \OuterIterator, FacetSetIterat
      */
     private $viewDisplayStrategy;
 
-    /**
-     * @param array|\Traversable $solariumFacetset
-     * @param CollatorInterface|null $collator
-     * @param callable $viewDisplayStrategy
-     **/
-    public function __construct($solariumFacetset, CollatorInterface $collator = null, $viewDisplayStrategy = null)
+    public function __construct(iterable $solariumFacetset, ?CollatorInterface $collator = null, ?callable $viewDisplayStrategy = null)
     {
-        if (null !== $viewDisplayStrategy && !is_callable($viewDisplayStrategy)) {
-            throw new \InvalidArgumentException('viewDisplayStrategy parameter must be callable if it is set.');
-        }
         $this->viewDisplayStrategy = $viewDisplayStrategy ?: function ($value) {
             return $value;
         };
+        if ($solariumFacetset instanceof \Traversable) {
+            $solariumFacetset = iterator_to_array($solariumFacetset);
+        }
         if ($collator) {
-            if ($solariumFacetset instanceof \Traversable) {
-                $solariumFacetset = iterator_to_array($solariumFacetset);
-            }
-            $this->count = count($solariumFacetset);
             uksort($solariumFacetset, function ($value1, $value2) use ($collator) {
                 return $collator->compare($value1, $value2);
             });
-            $this->facetValueIterator = new \ArrayIterator($solariumFacetset);
-
-            return;
         }
-        if (is_array($solariumFacetset)) {
-            $this->count = count($solariumFacetset);
-        } else {
-            $this->count = count(iterator_to_array($solariumFacetset));
-        }
-        if ($solariumFacetset instanceof \Iterator) {
-            $this->facetValueIterator = $solariumFacetset;
-        } elseif ($solariumFacetset instanceof \Traversable) {
-            $this->facetValueIterator = new \IteratorIterator($solariumFacetset);
-        } elseif (is_array($solariumFacetset)) {
-            $this->facetValueIterator = new \ArrayIterator($solariumFacetset);
-        } else {
-            throw new \InvalidArgumentException(sprintf('%s was passed a solarium_facetset parameter of type "%s", expecting a traversable object or an array.', __METHOD__, gettype($solariumFacetset)));
-        }
+        $this->count = count($solariumFacetset);
+        $this->facetValueIterator = new \ArrayIterator($solariumFacetset);
     }
 
     public function getInnerIterator()
